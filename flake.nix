ec86b39ceec619177a9f5fc3ce979e138991b488
Igor <igor@sgf.lt>
2025-05-04 16:53:18 +0300

    Return nix flake

---
+++ flake.nix
@@ -0,0 +1,32 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = nixpkgs.legacyPackages.${system};
+    in {
+      devShells = {
+        default = pkgs.mkShell {
+          packages = with pkgs; [
+            scala-next
+            metals
+            scalafmt
+            uv
+            ruff
+            alejandra
+          ];
+
+          shellHook = ''
+            source .env
+          '';
+        };
+      };
+    });
+}
